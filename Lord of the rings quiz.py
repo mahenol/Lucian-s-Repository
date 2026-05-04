ba70ee195bc98c0b7fbf6175b2f38a627a1454d2
Your Name <you@example.com>
2026-05-05 11:00:00 +1200

    quiz4

--- Lord of the rings quiz.py
+++ Lord of the rings quiz.py
@@ -3,12 +3,22 @@ famous movie franchise lord of the rings'''
 correct = 0
 highscore = 8
 wrong= 0
-#This part asks the first question
-first = input("How many members are in the Fellowship of the Ring? A/B/C \
-  A.Nine   B.Eleven   C.Seven " )
-#This part shows if the inputed answer was correct
-first = first.upper()
-if first == "A":
+index = 0
+#This is a list of all the questions
+questions = ["1.How many members are in the Fellowship of the Ring? A/B/C \
+                   A.Nine   B.Eleven   C.Seven ", 
+             "2.What is the name of the sword, broken and later reforged \
+for Aragorn? A/B/C      A.Excalibur   B.Narsil   C.Chunchunmaru ", 
+             "3.What creature did Gollum used to be before the ring corrupted \
+him? A/B/C              A.Borkle   B.Smeagol   C.Smaug"]
+#This is a list of all the answers
+answers = ["A", "B", "B"]
+
+for question in questions:
+   answer = input(question)
+answer = answer.upper()
+if answer == questions[index]:
+   index = index + 1
    correct += 1
 else:
    wrong +=  1
