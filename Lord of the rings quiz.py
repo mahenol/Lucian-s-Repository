b41cf9b5f9fac3fd7229597740f493cf284cef23
Your Name <you@example.com>
2026-05-04 13:25:20 +1200

    quiz3

--- Lord of the rings quiz.py
+++ Lord of the rings quiz.py
@@ -1,13 +1,15 @@
 '''This program quizes the user about the 
 famous movie franchise lord of the rings'''
-int = total_score = 0
-int = highscore = 8
-int = wrong = 0
+correct = 0
+highscore = 8
+wrong= 0
+#This part asks the first question
 first = input("How many members are in the Fellowship of the Ring? A/B/C \
   A.Nine   B.Eleven   C.Seven " )
+#This part shows if the inputed answer was correct
 first = first.upper()
 if first == "A":
-   total_score + 1
+   correct += 1
 else:
-   wrong + 1
-print(F"{total_score}")
+   wrong +=  1
+print(F"You got {correct}/{highscore}")
